c0121b798d60fc7cdd092672ec7e36c9846e7695
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-01-17 17:09:02 +0530

    ll

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // VITE_API_URL should be like: https://team-management-backend-iudd.onrender.com
-const API = `${import.meta.env.VITE_API_URL}/api`;
+const API = `${import.meta.env.VITE_API_URL}`;
 
 export default API;
